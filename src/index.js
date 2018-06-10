524d44e04cba58ceeb669089f01935a0a81082c2
SiobhanGi <siobhan.goggins@gmail.com>
2018-06-10 16:29:27 +0100

    added youtube api key

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyB5T4PkhFt0jpVFpU11H7mYFfftHjMsKRg';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => (<div>hi</div>);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component generated HTML and put it in the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+/*  <App /> = valid jsx
+
+    ReactDOM.render() Requires two arguemnts, the jsx app tag, and a dom selector.
+
+    Which is a reference to a node element on the html page.
+
+    This code is simply saying, reactDOM I want you to render 'app' in the
+    document at the node element that matches 'container'.
+
+    */
