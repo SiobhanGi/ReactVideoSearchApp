ad605b04bf7530135054a74b36d2b00f6e02ad4d
SiobhanGi <siobhan.goggins@gmail.com>
2018-06-10 19:58:16 +0100

    refactored index.js to class component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // libraries
-import React from 'react'; // go into node modules and get directory dom.
+import React, { Component } from 'react'; // go into node modules and get directory dom.
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -8,13 +8,27 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyB5T4PkhFt0jpVFpU11H7mYFfftHjMsKRg'; // youtube api key
 
-YTSearch( {key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
+// app
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
 
-const App = () => (<div>
-    <SearchBar />
-  </div>);
+    YTSearch( {key: API_KEY, term: 'scottish terriers'}, (data) => {
+      this.setState({ videos });
+      /* sets the state to the video data, the data name is a placeholder,
+      // if key and value are same in setState, we can condense to just
+       the name of the variable thanks to es6 */
+    });
+  }
+
+  render() {
+    return (<div>
+      <SearchBar />
+      </div>
+    );
+  };
+}
 
 // Take this component generated HTML and put it in the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
