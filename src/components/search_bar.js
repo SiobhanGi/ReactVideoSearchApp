de1e6bab6b05be39dc507e5c68fa5749899e4505
SiobhanGi <siobhan.goggins@gmail.com>
2018-06-10 19:43:54 +0100

    request to youtube api

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 
+// class component
 class SearchBar extends Component {
-  render() {
-    return <input onChange={(event) => console.log(event.target.value) }/>
+
+  // this is how we initialse state in a class component.
+  constructor(props) {
+    super(props);
+    this.state = { input: ''};
   }
 
-
+  render() {
+    return (
+    <div>
+    <input
+      value = { this.state.input }
+      onChange={ event => this.setState( { input: event.target.value }) }/>
+    </div>)
+  }
 }
 
 export default SearchBar;
+
+/*
+
+Controlled Field (Form/Input)- value is set by the state
+
+inside the input we've specificed that input has a value that equals to { this.state.term }
+so its value only changes when it state changes.
+
+The flow = this.setState sets the term to be the event.target.value. which then assigned to value what the event has recorded.
+this is how react handles data.
+*/
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 // libraries
 import React from 'react'; // go into node modules and get directory dom.
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 // component files
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyB5T4PkhFt0jpVFpU11H7mYFfftHjMsKRg'; // youtube api key
+
+YTSearch( {key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 const App = () => (<div>
     <SearchBar />
