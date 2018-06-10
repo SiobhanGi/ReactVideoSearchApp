f19f8d56f93853e53185ccbcbb67ddc14dbd012c
SiobhanGi <siobhan.goggins@gmail.com>
2018-06-10 20:57:34 +0100

    added markup to video-list-item.js

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-  return <li>Video</li>
+const VideoListItem = ({video}) => {
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading"></div>
+        </div>
+      </div>
+    </li>);
 };
 
 export default VideoListItem;
+
+
+/* ({video}) is es6 way of saying const video = props.video;
+it just cleans up code. */
