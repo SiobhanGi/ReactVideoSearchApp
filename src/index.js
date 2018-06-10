afb4116d909dc2fb2a8e731ada9a7c881ed5a30c
SiobhanGi <siobhan.goggins@gmail.com>
2018-06-10 23:38:37 +0100

    ran eslint and fixed what wish to fix

--- src/index.js
+++ src/index.js
@@ -5,46 +5,42 @@ import YTSearch from 'youtube-api-search';
 
 // component files
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 import VideoList from './components/video_list';
+
 
 // youtube api key
 const API_KEY = 'AIzaSyB5T4PkhFt0jpVFpU11H7mYFfftHjMsKRg';
 
-// app
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
 
-    YTSearch( {key: API_KEY, term: 'scottish terriers' }, (videos) => {
-      this.setState({ videos });
+    YTSearch({ key: API_KEY, term: 'scottish terriers' }, (videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
     });
   }
 
   render() {
-    return (<div>
-      <SearchBar />
-      <VideoList videos={ this.state.videos }/>
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
       </div>
     );
-  };
+  }
 }
-
-/* VideoList needs the list of videos, so we pass data from parent (app)
- to child (videolist) by defining a property on the JSX tag.
-
- render() {
-   return (<div>
-     <SearchBar />
-     <VideoList videos={ this.state.videos }/>
-     </div>
-
-This passing of data is passing 'props' or properties to videoList so
-we need to add argument of props to the videoList function.
-
-const VideoList = (props) => {}
-
-So anytime app renders videolist also gets the refresh.*/
 
 ReactDOM.render(<App />, document.querySelector('.container'));
