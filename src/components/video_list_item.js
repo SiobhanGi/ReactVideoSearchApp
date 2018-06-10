ae622de7e481c9ff7682c4488876f464babcc6b2
SiobhanGi <siobhan.goggins@gmail.com>
2018-06-10 21:02:10 +0100

    added video thumbnails and title to the video-list-item jsx markup

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,21 +2,25 @@ import React from 'react';
 
 const VideoListItem = ({video}) => {
 
+  const imgUrl = video.snippet.thumbnails.default.url;
+
   return (
     <li className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
-          <img className="media-object" />
+          <img className="media-object" src={imgUrl}/>
         </div>
 
         <div className="media-body">
-          <div className="media-heading"></div>
+          <div className="media-heading">{video.snippet.title}</div>
         </div>
       </div>
     </li>);
 };
 
 export default VideoListItem;
+
+// inside jsx to reference a js variable use curly braces.
 
 
 /* ({video}) is es6 way of saying const video = props.video;
